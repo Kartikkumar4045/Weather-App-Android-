d638b2c5d2fa2aec31015a403f23e7d9c052107b
Kartikkumar4045 <kumarskartik2@gmail.com>
2025-05-20 23:35:12 +0530

    Add some styling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     //chart depandancy
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("com.google.android.material:material:1.6.0")
+    implementation (libs.material.v1110)
 
 
 
